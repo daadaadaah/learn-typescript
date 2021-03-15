ffd074c624a6f5a1142123c8dd56c5b22358a49b
daadaadaah <daadaadaah@gmail.com>
2021-03-15 23:07:08 +0900

    1 type basic

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,34 @@
+// JS 문자열 선언
+// var str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+let num: number = 30;
+
+let arr: Array<number> = [1, 2, 3];
+
+let arr3: number[] = [1, 2, 3]; // 배열 리터럴로 타입 정의하기
+
+let arr2: Array<String> = ['a', 'b', 'c', 1];
+
+// TS 튜플 : 배열의 인덱스에 타입이 정의되어 있다.
+let address2: string[] = [];
+
+// TS 객체
+let obj: object = {};
+
+let person2: object = {
+  name : "dahee",
+  age: 1000,
+}
+
+let person: { name: string, age: number } = {
+  name : "dahee",
+  age: 1000,
+}
+
+// TS 진위값
+let isPerson: boolean = true;
+
+// 

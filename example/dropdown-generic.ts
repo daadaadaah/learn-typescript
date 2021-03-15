4c1daf5a4b46cc0f0e7e6453b72de7c41692c310
daadaadaah <daadaadaah@gmail.com>
2021-03-15 23:08:44 +0900

    Add dropdown

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,32 @@
-const emails = [
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: Dropdown<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+const numberOfProducts: Dropdown<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+// function createDropdownItem(item: Dropdown<string> | Dropdown<number>) {
+function createDropdownItem<T>(item: Dropdown<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -20,7 +36,13 @@ function createDropdownItem(item) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem<number>(product);
+  const selectTag = document.querySelector('#product-dropdown');
   selectTag.appendChild(item);
 });
